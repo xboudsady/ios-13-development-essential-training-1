84141b73a69b690fd3341f41bc0a6ea258154e0c
xboudsady <boudsadyx@gmail.com>
2019-12-28 18:28:27 -0800

    Completed Challenge 1.

--- Chapter 02/solution/start/Core Skills/Core Skills/ViewController.swift
+++ Chapter 02/solution/start/Core Skills/Core Skills/ViewController.swift
@@ -11,26 +11,30 @@ import UIKit
 class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var myLabel: UILabel!
     @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         myLabel.text = "Changed with Code!"
         myTextField.becomeFirstResponder()
         myTextField.delegate = self
+        lastNameTextField.delegate = self
     }
 
     @IBAction func buttonWasPressed(_ sender: Any) {
-        myLabel.text = "Hello \(myTextField.text!)!"
-        myTextField.resignFirstResponder()
+        myLabel.text = "Hello \(myTextField.text!) \(lastNameTextField.text!)!"
+        view.endEditing(true)
     }
+    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        myTextField.resignFirstResponder()
+        textField.resignFirstResponder()
         return false
     }
     
